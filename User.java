131a502b417ea12d9ded4bb3c9c10d510e39074a
JoaoFCosta <dinojoaocosta@gmail.com>
2015-05-08 00:52:34 +0100

    Add password hashing in User constructor
    Add password hashing when creating a new user in order to safely store the passwords

--- User.java
+++ User.java
@@ -4,7 +4,11 @@
  * 
  * @version 08/05/2015
  */
-import java.util.*;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class User {
     private String mail;              // User mail
@@ -32,7 +36,7 @@ public class User {
         this.statistics = new Statistic();
         this.friends = new ArrayList<User>();
     }
-    
+
     /**
      * Construtor with arguments
      * @arg mail User e-mail
@@ -41,23 +45,46 @@ public class User {
      */
     public User (String mail, String pass, String name) {
         this.mail = mail;
-        this.pass = pass;
         this.name = name;
+
+        // Encrypt password
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(pass.getBytes());
+
+            byte byteData[] = md.digest();
+
+            StringBuffer sb = new StringBuffer();
+
+            for (int i = 0; i < byteData.length; i++)
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+
+            this.pass = sb.toString();
+        } catch (NoSuchAlgorithmException e){
+            this.pass = null;
+        }
     }
-    
+
     // Getters
-    
+
     /**
      * @return User mail
      */
     public String getMail () {
         return this.mail;
     }
-    
+
     /**
      * @return User name
      */
     public String getName () {
         return this.name;
+    }
+    
+    /**
+     * @return User password
+     */
+    public String getPass () {
+        return this.pass;
     }
 }
