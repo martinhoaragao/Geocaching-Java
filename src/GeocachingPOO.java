370b409324b458908bc8fa32ade825b4d272b072
JoaoFCosta <dinojoaocosta@gmail.com>
2015-05-08 14:12:41 +0100

    Add main function to GeocachingPOO and option to register new user, add functions to UserBase
    Add main function to GeocachingPOO.java and 'menu' and 'register' functions to display a menu and register a new user given an e-mail, username and password
    Add functions to 'src/UserBase.java' to create an UserBase object, add a new user to the UserBase, check if a e-mail is already in use, get an User from the UserBase given the password and a function to encrypt the password to see if it matches the one stored in the User

--- src/GeocachingPOO.java
+++ src/GeocachingPOO.java
@@ -5,14 +5,62 @@
  * All the classes and all the methods created for User-Application will be avaiable here.
  */
 
+import java.util.Scanner;
+
 public class GeocachingPOO {
-    private User user;    // The information of the User that has been login in.
+    private User user = null;           // User that is logged in
+    private static UserBase userbase = null;   // User data base
     
     //Random main method/function to complete.
-    public static int main(String[] args){
-     System.out.println("Hello");
-     return 0;
+    public static int main() {
+        boolean running = true;
+        int option = 0;
+        userbase = new UserBase();
+        
+        while (running) {
+            option = menu();
+            switch (option) {
+                case 1: register(); break;
+                case 2: running = false; break;
+            }
+        }
+        
+        return 0;
     }
     
+    /**
+     * Display the menu and return the user choice
+     */
+    private static int menu () {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("1: Register");
+        System.out.println("2: Exit");
+        
+        return sc.nextInt();
+    }
     
+    /**
+     * Function to register new user
+     */
+    private static void register () {
+        Scanner sc = new Scanner(System.in);
+        String mail, pass, name;
+        User newuser;
+        
+        System.out.print("E-mail: ");
+        mail = sc.nextLine();
+        System.out.print("Name: ");
+        name = sc.nextLine();
+        System.out.print("Password: ");
+        pass = sc.nextLine();
+        
+        if (userbase.exists(mail)) {    // E-maill already in use
+            System.out.println("E-mail already in use");
+        } else {
+            newuser = new User(mail, pass, name);
+            userbase.addUser(newuser);
+            newuser = null;
+        }
+    }
 }
--- src/UserBase.java
+++ src/UserBase.java
@@ -1,9 +1,74 @@
 /**
  * Classe to save information of all the Users that have been created.
+ * 
+ * @version 08/05/2015
  */
 
 import java.util.TreeMap;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class UserBase {
     private TreeMap<String, User> users;
+
+    /**
+     * Constructor withtout arguments
+     */
+    public UserBase () {
+        this.users = new TreeMap<String, User>();
+    }
+
+    /**
+     * Add a user to the data base
+     * @arg user User to be added
+     */
+    public void addUser (User user) {
+        this.users.put(user.getMail(), user);
+    }
+
+    /**
+     * Get User given an e-mail and a password
+     * @arg mail User mail
+     * @arg pass User password
+     */
+    public User getUser (String mail, String pass) {
+        User aux = this.users.get(mail);
+
+        if (aux == null) { return null; }   // No user with the given mail
+        else {
+            if (encryptPass(pass).equals(aux.getPass()))
+                return aux;
+            else
+                return null;
+        }
+    }
+
+    /**
+     * Encrypt a password the same way the User class does
+     * @arg pass Password to be encrpyted
+     */
+    private String encryptPass (String pass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(pass.getBytes());
+
+            byte byteData[] = md.digest();
+            StringBuffer sb = new StringBuffer();
+
+            for (int i = 0; i < byteData.length; i++)
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e){   // Unable to ecnrypt password
+            return pass;
+        }
+    }
+
+    /**
+     * Check if a give e-mail already has a user associated
+     * @arg mail
+     */
+    public boolean exists (String mail) {
+        return this.users.containsKey(mail);
+    }
 }
