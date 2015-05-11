4c58b5383a2cc19858cc138f173dc7af4509fa83
Jessica <a71164@alunos.uminho.pt>
2015-05-11 15:46:08 +0100

    Created field, constructor and method for Server.

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * A class to represent the Server. 
  * Only the Server has full access to all Data-Bases (Caches and Users).
@@ -7,4 +9,19 @@
 
 public class Server
 {
+    private Scanner reader;
+    
+    public Server(){
+        reader = new Scanner(System.in);
+    }
+    
+    
+    public String getInput()
+    {
+        System.out.print("> ");         // print prompt
+        String inputLine = reader.nextLine();
+
+        return inputLine;
+    }
+    
 }
