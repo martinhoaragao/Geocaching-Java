df036133d222f8c05a7ba25ffe9e820174a9f1ef
JoaoFCosta <dinojoaocosta@gmail.com>
2015-05-12 15:56:15 +0100

    Add constructors and methods to CacheBase
    Add empty constructor and constructor by cloning to 'src/CacheBase.java'
    Add toString, equals and clone to 'src/CacheBase.java'
    Add getAllCaches, getCaches, getNumOfCaches, addCache and exists to 'src/CacheBase.java'

--- src/CacheBase.java
+++ src/CacheBase.java
@@ -1,12 +1,130 @@
 /**
  * CacheBase represents the Data-Base for all the Caches that have been created (by the server
  * or by users).
- * 
+ *
  * @version 07/05/2015
  */
 
 import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CacheBase {
     private TreeSet<Cache> caches;    // All the existing Caches.
+
+    /**
+     * Constructor without arguments
+     */
+    public CacheBase () {
+      this.caches = new TreeSet<Cache>();
+    }
+
+    /**
+     * Constructs a UserBase with the caches present on another UserBase
+     * @arg cbase CacheBase from where the caches will be copied
+     */
+    public CacheBase (CacheBase cbase) {
+      TreeSet<Cache> ts = cbase.getAllCaches();
+      Iterator it = ts.iterator();
+
+      while (it.hasNext())
+        this.caches.add((Cache) it.next());
+    }
+
+    // Getters
+
+    /**
+     * @return TreeSet with all the caches stored on the CacheBase
+     */
+    public TreeSet<Cache> getAllCaches () {
+      return (TreeSet<Cache>) this.caches.clone();
+    }
+
+    /**
+     * @return ArrayList with all the caches owned by a given e-mail
+     * @arg mail Owner e-mail
+     */
+    public ArrayList<Cache> getCaches (String mail) {
+      ArrayList<Cache> result = new ArrayList<Cache>();
+      Iterator it = this.caches.iterator();
+
+      while (it.hasNext()) {
+        Cache aux = (Cache) it.next();
+        if (aux.getMail().equals(mail)) result.add(aux);
+      }
+
+      return result;
+    }
+
+    /**
+     * @return Number of caches stored in the CacheBase
+     */
+    public int getNumOfCaches () {
+      return this.caches.size();
+    }
+
+    /**
+     * Add a cache to the CacheBase
+     * @arg cache Cache to be added
+     */
+    public void addCache (Cache cache) {
+      this.caches.add(cache);
+    }
+
+    /**
+     * Check if a given Cache is present in the Cache data base
+     * @arg cache
+     */
+    public boolean exists (Cache cache) {
+      Iterator it = this.caches.iterator();
+      boolean found = false;
+
+      while (it.hasNext() && !found)
+        found = cache.equals((Cache) it.next());
+
+      return found;
+    }
+
+    // toString, equals and clone
+
+    /**
+     * Return CacheBase info in String
+     */
+    public String toString () {
+      StringBuilder sb = new StringBuilder();
+      Iterator it = this.caches.iterator();
+      
+      sb.append(this.caches.size() + " caches are stored.\n");
+
+      while (it.hasNext()) {
+        Cache aux = (Cache) it.next();
+        Coordinates coords = (Coordinates) aux.getCoords();
+        sb.append("Lat: " + coords.getLat());
+        sb.append(" Lon: " + coords.getLon() + "\n");
+      }
+
+      return sb.toString();
+    }
+
+    /**
+     * Compares this CacheBase to another CacheBase to check if they are equal
+     * @arg cbase CacheBase to use for comparison
+     */
+    public boolean equals (Object cbase) {
+      if (cbase == this) return true;
+
+      if ((cbase == null) || (cbase.getClass() != this.getClass())) return false;
+
+      CacheBase aux = (CacheBase) cbase;
+      boolean comp = (this.caches.size() == aux.getNumOfCaches());
+
+      if (comp) {
+        Iterator it = this.caches.iterator();
+
+        while (it.hasNext() && comp)
+          comp = comp && (aux.exists((Cache) it.next()));
+      }
+
+      return comp;
+    }
 }
