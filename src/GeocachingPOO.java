28f77ceb8554de8a2123b3ccab94d8096a51cbf2
JoaoFCosta <dinojoaocosta@gmail.com>
2015-05-18 22:40:35 +0100

    Change GeocachingPOO so that it can run on BlueJ

--- src/GeocachingPOO.java
+++ src/GeocachingPOO.java
@@ -57,7 +57,6 @@ public class GeocachingPOO {
     /** Display the main menu and return the user choice */
     private static int mainMenu () {
         Scanner sc = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");  // Clear terminal view
 
         System.out.println("1: Register");
         System.out.println("2: Login");
@@ -69,7 +68,6 @@ public class GeocachingPOO {
     /** Auxiliary function to display user menu */
     private static int userMenu () {
         Scanner sc = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");  // Clear terminal view
 
         System.out.println("Logged in as: " + user.getName() + " | " + user.getPoints() + " points");
         System.out.println("1: Personal Information");
@@ -88,7 +86,6 @@ public class GeocachingPOO {
     /** Auxiliary function to register new user */
     private static void register () {
         Scanner sc = new Scanner(System.in);
-        Console console = System.console();
         String name, pass, country, city, gender, mail = "";
         String[] bdate_fields;
         GregorianCalendar bdate = null;
@@ -96,7 +93,6 @@ public class GeocachingPOO {
         MailValidator mv = new MailValidator();
         boolean aux = true;
 
-        System.out.print("\033[H\033[2J");  // Clear terminal view
         do {
             if (!aux) System.out.println("Invalid e-mail!");
             System.out.print("E-mail: ");
@@ -116,8 +112,8 @@ public class GeocachingPOO {
         bdate = new GregorianCalendar(Integer.parseInt(bdate_fields[2]),
         Integer.parseInt(bdate_fields[1]), Integer.parseInt(bdate_fields[0]));
 
-        pass = new String(console.readPassword("Password: "));
-
+        System.out.print("Pass: ");
+        pass = sc.nextLine().replaceAll("[\n\r]","");
         System.out.print("Country: ");
         country = sc.nextLine().replaceAll("[\n\r]","");
         System.out.print("City: ");
@@ -134,18 +130,14 @@ public class GeocachingPOO {
             newuser.setGender(gender);
             newuser = null; id++;
             System.out.println("User sucessfuly created!");
-            if (console != null) console.readLine();
         }
     }
 
     /** Auxiliary login function */
     private static void login () {
         Scanner sc = new Scanner(System.in);
-        Console console = System.console();
         String mail, pass;
         int i = 0;
-
-        System.out.print("\033[H\033[2J");  // Clear terminal view
 
         do {
             System.out.print("E-mail: ");
@@ -159,30 +151,21 @@ public class GeocachingPOO {
             } else user = userbase.getUser(mail, pass);
             i++;
         } while ((user == null) && (i != 3));
-
-        if (console != null) console.readLine();
     }
 
     /** Auxiliary function to print user information */
     private static void printInfo () {
-        Console c = System.console();
-
-        System.out.print("\033[H\033[2J"); // Clear terminal
         System.out.println(user.toString());
-
-        if (c != null) {
-            c.readLine();
-        }
     }
 
     /** Auxiliary function to change user password */
     private static void changePassword() {
-        Console console = System.console();
         String currentpass, newpass;
+        Scanner sc = new Scanner(System.in);
         int i = 0; //3 trys to change password each time
 
-        System.out.print("\033[H\033[2J");  // Clear terminal view
-        currentpass = new String(console.readPassword("Current password: "));
+        System.out.println("Current Password: ");
+        currentpass = sc.nextLine().replaceAll("[\n\r]","");
 
         // Give user 3 tries to inser current password
         while(i<3 && !user.confirmPass(currentpass)) {
@@ -193,14 +176,15 @@ public class GeocachingPOO {
             }
             i++;
 
-            currentpass = new String(console.readPassword("Current password: "));
+            System.out.println("Current Password: ");
+            currentpass = sc.nextLine().replaceAll("[\n\r]","");
             if(i==3)System.out.println(" Passwords don't match! ");
         }
 
         //If User sucessfully types current password, he may change it
         if(user.confirmPass(currentpass)){
             System.out.println("Type new password: ");
-            newpass = new String(console.readPassword("New Password: "));
+            newpass = sc.nextLine().replaceAll("[\n\r]","");
 
             userbase.getUser(user.getMail(),currentpass).setPass(newpass);
         }
@@ -209,7 +193,6 @@ public class GeocachingPOO {
     /** Auxiliary function to change User name */
     private static void changeName () {
         Scanner sc = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");  // Clear terminal view
 
         System.out.print("Name: ");
         user.setName(sc.nextLine().replaceAll("[\n\r]", ""));
@@ -220,7 +203,6 @@ public class GeocachingPOO {
         Scanner sc = new Scanner(System.in);
         String city, country;
 
-        System.out.print("\033[H\033[2J");  // Clear terminal view
         System.out.print("City: ");
         city = sc.nextLine().replaceAll("[\n\r]", "");
         System.out.print("Country: ");
@@ -233,7 +215,6 @@ public class GeocachingPOO {
         Scanner sc = new Scanner(System.in);
         String[] bdate;
 
-        System.out.print("\033[H\033[2J");  // Clear terminal view
         System.out.print("Birthdate (Day/Month/Year): ");
         user.setBDate(sc.nextLine().replaceAll("[\n\r]", ""));
     }
@@ -242,7 +223,6 @@ public class GeocachingPOO {
     private static void changeGender () {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("\033[H\033[2J");  // Clear terminal view
         System.out.print("Gender: ");
         user.setGender(sc.nextLine().replaceAll("[\n\r]", ""));
     }
@@ -250,9 +230,7 @@ public class GeocachingPOO {
     /** Auxiliary function to add a user as friend */
     private static void addFriend () {
         Scanner sc = new Scanner(System.in);
-        Console c = System.console();
         User u;
-        System.out.print("\033[H\033[2J");  // Clear terminal
 
         u = userbase.getUserInfo(sc.nextLine().replaceAll("[\n\r]", ""));
         if (u != null) {
@@ -262,14 +240,11 @@ public class GeocachingPOO {
             System.out.println("No User with such e-mail");
         }
 
-        if (c != null) c.readLine();
     }
 
     /** Auxiliary function to show friends */
     private static void showFriends () {
-        Console c = System.console();
         User u;
-        System.out.print("\033[H\033[2J");  // Clear terminal
 
         ArrayList<Integer> list = user.getFriends();
 
@@ -278,8 +253,5 @@ public class GeocachingPOO {
             if (u != null)
                 System.out.println(u.getName() + " - " + u.getMail());
         }
-
-
-        if (c != null) c.readLine();
     }
 }
