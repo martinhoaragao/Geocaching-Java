93f44488681d7d276c63c222f1754fb994ba38d2
Jessica <a71164@alunos.uminho.pt>
2015-05-24 16:36:54 +0100

    Add Exception for Puzzle but I also have other idea
    Instead of treating exceptions we can have the sets has public boolean
    set(int star)    if star is valid range , sets it and returns true,
    otherwise returns false and does not set it. Then in geocaching poo we
    send message to user if return value is false.

---
+++ src/Exceptions/PuzzleStarEx.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class PuzzleStarEx extends Exception{
+
+    public PuzzleStarEx(){
+        super();
+    }
+
+    public PuzzleStarEx(String msg){
+        super(msg);
+    }
+    
+}
