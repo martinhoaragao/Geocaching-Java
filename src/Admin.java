dc66ef6d8d89a045e5230431019799f844a3e8fc
Martinho Aragão <martinhoaragao@gmail.com>
2015-05-24 16:44:16 +0100

    Split users in admin and user. Add inheritance. Reduce equals to class and mail

--- src/Admin.java
+++ src/Admin.java
@@ -12,45 +12,27 @@ import java.util.Iterator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Admin {
-    private String name;              // Admin username
-    private String pass;              // Admin Password
-    private Double id;                // Admin id
+public class Admin extends User{
     private Integer permi;            // Permission level (0 - lowest or >0)
 
     /**
      * Constructor without arguments
      */
     public Admin () {
-        this.name = new String();
-        this.pass = new String();
-        this.id = 0.0;
+        super();
         this.permi = 0;
     }
 
     /**
      * Construtor with arguments
+     * @param mail  Admin e-mail
      * @param name  Admin username
      * @param pass  Admin password
      * @param id    Admin id
      * @param permi Admin permissions
      */
-    public Admin (String name, String pass, Double id, Integer permi) throws NullPointerException, IllegalStateException {
-        this();
-
-        if (pass == null)
-            throw new NullPointerException("pass can't be null!");
-        if (pass.trim() == "")
-            throw new IllegalStateException("pass can't be empty!");
-        this.pass = encryptPass(pass);
-
-        if (name == null)
-            throw new NullPointerException("name can't be null!");
-        if (name.trim() == "")
-            throw new IllegalStateException("name can't be empty!");
-        this.name = name;
-
-        this.id = id;
+    public Admin (String email, String name, String pass, Double id, Integer permi) throws NullPointerException, IllegalStateException {
+        super(email, pass, name, id);
 
         if (permi < 0)
             throw new IllegalStateException("Illegal permission level!");
@@ -62,39 +44,11 @@ public class Admin {
      * @param user User from which the information will be fetched
      */
     public Admin (Admin admin) throws NullPointerException {
-        this();
-
-        if (admin == null)
-            throw new NullPointerException("user can't be null!");
-
-        this.name = admin.getName();
-        this.pass = admin.getPass();
-        this.id = admin.getId();
+        super();
         this.permi = admin.getPermi();
     }
 
     // Getters
-
-    /**
-     * @return Admin username
-     */
-    public String getName () {
-        return this.name;
-    }
-
-    /**
-     * @return Admin encrypted password
-     */
-    public String getPass () {
-        return this.pass;
-    }
-
-    /**
-     * @return Admin Id
-     */
-    public Double getId () {
-        return this.id;
-    }
 
     /**
      * @return Admin permission level
@@ -104,44 +58,6 @@ public class Admin {
     }
 
     //Setters
-
-    /**
-     * Chanches the Admin username
-     */
-    public void setName (String name) throws NullPointerException, IllegalStateException {
-        if (name == null)
-            throw new NullPointerException("name can't be null!");
-        if (name.trim() == "")
-            throw new IllegalStateException("name can't be empty");
-        this.name = name;
-    }
-
-    /**
-     * Set the password for the first time
-     * @param passw Password for the admin
-     */
-    public void setPass (String passw) throws NullPointerException, IllegalStateException {
-        if (passw == null)
-            throw new NullPointerException("passw can't be null!");
-        if (passw.trim() == "")
-            throw new IllegalStateException("passw can't be empty!");
-
-        this.pass = encryptPass(passw);
-    }
-
-
-    /**
-     * Change the admin id
-     * @param id New admin id
-     */
-    public void setId (Double id) throws NullPointerException, IllegalStateException {
-        if (id == null)
-          throw new NullPointerException("id can't be null!");
-        if (id < 0)
-          throw new IllegalStateException("id can't be negative!");
-
-        this.id = id;
-    }
 
     /**
      * Change the admin permission level
@@ -161,10 +77,8 @@ public class Admin {
      */
     public String toString () {
         StringBuilder sb = new StringBuilder();
-        String day, month, year;
 
-        sb.append("Name: " + this.name + "\n");
-        sb.append("Admin id: " + this.id.intValue() + "\n");
+        sb.append(super.toString());
         sb.append("Admin has level: " + this.permi + "\n");
 
         return sb.toString();
@@ -183,58 +97,14 @@ public class Admin {
         if ((admin == null) || (this.getClass() != admin.getClass())) return false;
 
         Admin aux = (Admin) admin;
-        boolean comp = this.name.equals(aux.getName());
-        comp = comp && (this.pass.equals(aux.getPass()));
-        comp = comp && (this.permi.equals(aux.getPermi()));
-        return comp;
+        return this.getMail().equals(aux.getMail());
     }
 
     /**
      * Create a clone of this object
      */
+    @Override
     public Admin clone () {
         return new Admin(this);
-    }
-
-    // Other methods
-
-    /**
-     * Confirm if the given password is equal to the one stored
-     * @param pass
-     */
-    public boolean confirmPass (String pass) throws NullPointerException, IllegalStateException {
-        if (pass == null)
-            throw new NullPointerException("pass can't be null!");
-        if (pass.trim() == "")
-            throw new IllegalStateException("pass can't be empty!");
-
-        if (this.getPass().equals(encryptPass(pass))) return true;
-        else return false;
-    }
-
-    /**
-     * Function to encrypt password when creating admin
-     * @param pass Password to be encrypted
-     */
-    private String encryptPass (String pass) throws NullPointerException, IllegalStateException {
-        if (pass == null)
-            throw new NullPointerException("pass can't be null!");
-        if (pass.trim() == "")
-            throw new IllegalStateException("pass can't be empty!");
-
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(pass.getBytes());
-
-            byte byteData[] = md.digest();
-            StringBuffer sb = new StringBuffer();
-
-            for (int i = 0; i < byteData.length; i++)
-                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e){   // Unable to encrypt password
-            return pass;
-        }
     }
 }
---
+++ src/NormalUser.java
@@ -0,0 +1,325 @@
+/**
+ * Class to represent a user with some personal information, the user activities,
+ * statistics and friends
+ *
+ * @version 08/05/2015
+ */
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class NormalUser extends User {
+    private boolean gender;           // User gender
+    private Address address;          // User Address
+    private GregorianCalendar bdate;  // User birthdate
+    private int points;               // User points
+
+    private TreeSet<Activity> activities;       // Last 10 User activities
+    private Statistic statistics;               // User statistics
+    private ArrayList<Double> friends;          // User friends
+    private ArrayList<Double> friend_requests;  // Friend Requests
+
+    /**
+     * Constructor without arguments
+     */
+    public NormalUser () {
+        super();
+        this.gender = true;
+        this.address = new Address("New York","USA");
+        this.bdate = new GregorianCalendar();
+        this.points = 0;
+        this.activities = new TreeSet<Activity>(new CacheDateComparator());
+        this.statistics = new Statistic();
+        this.friends = new ArrayList<Double>();
+        this.friend_requests = new ArrayList<Double>();
+    }
+
+    /**
+     * Construtor with arguments
+     * @param mail  NormalUser e-mail
+     * @param pass  NormalUser password
+     * @param name  NormalUser name
+     * @param id    NormalUser id
+     * @param bdate NormalUser birthdate
+     */
+    public NormalUser (String mail, String pass, String name, Double id, GregorianCalendar bdate) throws NullPointerException, IllegalStateException {
+        super(mail, pass, name, id);
+
+        this.address = new Address();
+        this.bdate = (GregorianCalendar) bdate.clone();
+
+        this.points = 0;
+        this.activities = new TreeSet<Activity>(new CacheDateComparator());
+        this.statistics = new Statistic();
+        this.friends = new ArrayList<Double>();
+        this.friend_requests = new ArrayList<Double>();
+    }
+
+    /**
+     * Construct a new NormalUser with the same info as a given NormalUser
+     * @param user NormalUser from which the information will be fetched
+     */
+    public NormalUser (NormalUser user) throws NullPointerException {
+        super(user);
+
+        this.bdate = user.getBDate(); this.points = user.getPoints();
+        this.activities = user.getLastActivities();
+        this.statistics = user.getStatistics();
+        this.friends = user.getFriends();
+    }
+
+    // Getters
+
+    /**
+     * @return User gender
+     */
+    public boolean getGender () {
+        return this.gender;
+    }
+
+    /**
+     * @return User Address
+     */
+    public Address getAddress () {
+        return this.address.clone();
+    }
+
+    /**
+     * @return User birthdate
+     */
+    public GregorianCalendar getBDate () {
+        return (GregorianCalendar) this.bdate.clone();
+    }
+
+    /**
+     * @return User points
+     */
+    public Integer getPoints () {
+        return this.points;
+    }
+
+    /**
+     * @return TreeSet with last 10 user activities
+     */
+    @SuppressWarnings("unchecked")
+    public TreeSet<Activity> getLastActivities () {
+      TreeSet<Activity> ts = new TreeSet<Activity>(new CacheDateComparator());
+      Iterator it = activities.iterator();
+      Activity aux;
+
+      while (it.hasNext()) {
+        aux = (Activity) it.next();
+        ts.add(aux.clone());
+      }
+      return ts;
+    }
+
+    /**
+     * @return User statistics
+     */
+    public Statistic getStatistics () {
+        return this.statistics.clone();
+    }
+
+    //Setters
+
+
+    /**
+     * Change the gender
+     */
+    public void setGender (boolean gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * Change the adress
+     */
+    public void setAddress (String city, String country) throws NullPointerException, IllegalStateException {
+
+        // Exceptions
+        if (city == null)
+            throw new NullPointerException("city can't be null!");
+        if (city.trim() == "")
+            throw new IllegalStateException("city can't be empty!");
+        if (country == null)
+            throw new NullPointerException("country can't be null!");
+        if (country.trim() == "")
+            throw new IllegalStateException("country can't be empty!");
+
+        this.address = new Address(city, country);
+    }
+
+
+    /**
+     * Change the user birthdate
+     * @param date Date formated as 'DD/MM/YY'
+     * @return 0 if valid date, 1 if invalid day, 2 if invalid month, 3 if invalid year
+     */
+    public int setBDate (String date) throws NullPointerException, IllegalStateException {
+        String[] parts;
+        int return_value = 0;
+
+        // Exceptions
+        if (date == null)
+            throw new NullPointerException("date can't be null!");
+        if (date.trim() == "")
+            throw new IllegalStateException("date can't be empty!");
+
+        parts = date.split("/");
+
+        if (parts.length != 3)
+            throw new IllegalStateException("date is in wrong format!");
+
+        int d = Integer.parseInt(parts[0]);
+        int m = Integer.parseInt(parts[1]);
+        int y = Integer.parseInt(parts[2]);
+
+        this.bdate = new GregorianCalendar(y,m,d);
+
+        if (d != this.bdate.get(GregorianCalendar.DAY_OF_MONTH))
+            return_value = 1;
+        else if (m != this.bdate.get(GregorianCalendar.MONTH))
+            return_value = 2;
+        else if  (y != this.bdate.get(GregorianCalendar.YEAR))
+            return_value = 3;
+
+        return return_value;
+    }
+
+    /**
+     * Change the user birthdate
+     * @param date Date formated as GregorianCalendar
+     */
+    public void setBDate(GregorianCalendar date) {
+        //"03/05/1994"
+        GregorianCalendar bbb = (GregorianCalendar) date.clone();
+        this.bdate = bbb;
+    }
+
+    // toString, equals and clone
+
+    /**
+     * Transform user info into a String
+     */
+    public String toString () {
+        StringBuilder sb = new StringBuilder();
+        String day, month, year;
+
+        sb.append(super.toString());
+        if (bdate != null) {
+            day = String.valueOf(bdate.get(GregorianCalendar.DAY_OF_MONTH));
+            month = String.valueOf(bdate.get(GregorianCalendar.MONTH));
+            year = String.valueOf(bdate.get(GregorianCalendar.YEAR));
+            sb.append("Birthdate: " + day + "/" + month + "/" + year + "\n");
+        }
+        sb.append("Gender: " + (this.gender ? "F" + "\n" : "M" + "\n"));
+        sb.append("Address: " + this.address.toString());
+
+        return sb.toString();
+    }
+
+    /**
+     * Compares this object with another User to check if they are equal
+     * @param user User to compare with
+     */
+    public boolean equals (Object user) throws NullPointerException {
+        if (user == null)
+            throw new NullPointerException("user can't be null!");
+
+        if (this == user) return true;
+
+        if ((user == null) || (this.getClass() != user.getClass())) return false;
+
+        NormalUser aux = (NormalUser) user;
+        return this.getMail().equals(aux.getMail());
+    }
+
+    /**
+     * Create a clone of this object
+     */
+    @Override
+    public NormalUser clone () {
+        return new NormalUser(this);
+    }
+
+    /* -------------------------------- FRIENDS -----------------------------*/
+
+    /** Add a User id to the friends request
+     * @param id The user id
+     */
+    public void addFriendRequest (Double id) throws IllegalArgumentException {
+      if (id < 0)
+        throw new IllegalArgumentException("id has to be positive!");
+
+      this.friend_requests.add(id);
+    }
+
+    /** Remove User id from the friends request
+     * @param id Id to be removed
+     */
+    public void removeFriendRequest (Double id) throws IllegalArgumentException {
+        if (id < 0) /* Invalid id */
+            throw new IllegalArgumentException("id has to be positive.");
+
+        this.friend_requests.remove(id);
+    }
+
+    /**
+     * Add a user as a friend
+     * @param id User to be added id
+     */
+    public void addFriend (Double id) throws IllegalArgumentException {
+        if (id < 0) /* Invalid id */
+            throw new IllegalArgumentException("id has to be positive.");
+
+        this.friends.add(id);
+    }
+
+    /**
+     * @return User friends list
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayList<Double> getFriends () {
+        return (ArrayList<Double>) this.friends.clone();
+    }
+
+    /** @return Friend Requests list */
+    @SuppressWarnings("unchecked")
+    public ArrayList<Double> getFriendRequests () {
+        return (ArrayList<Double>) this.friend_requests.clone();
+    }
+
+    /* ---------------------------- ACTIVITIES --------------------------- */
+
+    /** Add an Activity to the activities queue
+     * and automaticaly to the statistics
+     * @param act Activity to be added
+     */
+    public void addActivity (Activity act) throws NullPointerException {
+      Activity aux = null;
+
+      if (act == null)
+        throw new NullPointerException("act can't be null!");
+
+      /* Remove head if there are already 10 activities */
+      if (this.activities.size() == 10)
+        try {
+            aux = this.activities.first();
+        } catch (Exception e) { aux = null; }
+      if (aux != null) this.activities.remove(aux);
+
+      this.activities.add(act);
+      this.points += act.getPoints();
+
+      this.statistics.addAct(act);
+
+
+    }
+
+    //TODO  ADICIONAR ESTATISTICA ACTIVIDADE AO ANO TAMBÉM
+}
