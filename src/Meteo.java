bd03f49bc6e26dac97d8d252efd1286f8cb00b34
Jessica <a71164@alunos.uminho.pt>
2015-05-27 13:23:38 +0100

    Add class Meteo and test with it. It makes randomly meteo's and calculates the points given that meteo class fields.

---
+++ src/Meteo.java
@@ -0,0 +1,206 @@
+/**
+ * This class represents the Meteorology to be used in Activity.
+ * This will calculate points depending on the temperature, time_state, etc...
+ * And sets these fields in the constructors arbitrarily.
+ */
+
+import java.util.Random;
+
+public class Meteo{
+    
+    private int temperature; 
+    private int weather;
+
+    private static int limit_points = 20;
+    /* 
+        Rainy 0
+        Stormy 1
+        Sunny 2
+        Cloudy 3
+        Windy 4
+        Foggy 5
+        Hail 6 
+    */
+
+    public Meteo(){
+        Random temperaturer = new Random();
+        int Low = -10;
+        int High = 50;
+        int R = temperaturer.nextInt(High-Low) + Low;
+        this.temperature = R;
+
+        Random w = new Random();
+        int W = w.nextInt(7); // 0,1,2,3,4,5,6   7 exclusive
+
+
+        this.weather = W;
+    }
+
+    public Meteo(int temperature, int weather){
+        this.temperature = temperature;
+        this.weather = weather;
+    }
+
+    public Meteo(Meteo a){
+    	this.temperature = a.getTemp();
+    	this.weather = a.getWeather();
+    }
+
+    /* auxiliary functions to calculate points for weather */
+    public int calcPointsW(){
+    	int p=0;
+		int w = this.getWeather();
+		switch (w){
+
+            case 0: p+=9 ;
+            break;
+
+            case 1:  p+=10;
+            break;
+
+            case 2:  p+=4;
+            break;
+
+            case 3:  p+=6;
+            break;
+
+            case 4:  p+=8;
+            break;
+
+            case 5:  p+=7;
+            break;
+
+            case 6:  p+=10;
+            break;
+
+            default:
+            break;
+        /* 
+        Rainy 0
+        Stormy 1
+        Sunny 2
+        Cloudy 3
+        Windy 4
+        Foggy 5
+        Hail 6 
+		*/
+
+    	}
+    	return p;
+    }
+
+    /* auxiliary functions to calculate points for temperature */
+    public int calcPointsT(){
+    	int p =0;
+    	int t = this.getTemp();
+
+    	if(t <= -5) p+=10;
+    	else if(t>=30) p+=10;
+    	else if(t> -5 && t < 0) p+=8;
+    	else if(t>16 && t<25 ) p+=4;
+    	else if(t>=0 && t <=15) p+=6;
+    	else p+=7;
+    		//25->30
+
+        return p;
+    }
+
+    public int calcPoints(){
+    	int p=0;
+    	p+=calcPointsT();
+    	p+=calcPointsW();
+
+    	return p;
+
+    	
+    }
+
+    //Gets e Sets
+    public int getWeather(){
+        return this.weather;
+    }
+
+    public int getTemp(){
+        return this.temperature;
+    }
+    
+    public void setTemp(int temperature){
+        this.temperature = temperature;
+    }
+    
+    public void setWeather(int weather){
+        this.weather = weather;
+    }
+
+    /* auxiliary function to get String of weather */
+    /*Rainy 0
+        Stormy 1
+        Sunny 2
+        Cloudy 3
+        Windy 4
+        Foggy 5
+        Hail 6*/ 
+    private String getStringW(){
+        int w = this.weather;
+        String info = new String();
+        switch (w){
+
+            case 0: info =new String( "Rainy");
+            break;
+
+            case 1:  info = new String("Stormy");
+            break;
+
+            case 2: info =new  String( "Sunny");
+            break;
+
+            case 3: info =new  String( "Cloudy");
+            break;
+
+            case 4:info = new String( "Windy");
+            break;
+
+            case 5:info =new  String("Foggy");
+            break;
+
+            case 6: info = new String("Hail");
+            break;
+
+            default:
+            break;
+
+            
+        }
+        
+        return info;
+
+
+    }
+
+    /* toString, equals, clone */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Temperature: ");
+        sb.append(this.getTemp());
+
+        sb.append("Weather conditions: ");
+        sb.append(this.getStringW());
+
+        return sb.toString();
+    }
+
+    public boolean equals(Object o){
+    	if(this == o) return true;
+
+    	if(this.getClass() != o.getClass()) return false;
+
+    	Meteo a = (Meteo) o;
+    	return a.getTemp() == this.getTemp() && a.getWeather() == this.getWeather();
+    }
+
+    public Meteo clone(){
+    	return new Meteo(this);
+
+    }
+}
